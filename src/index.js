14088777378fb4a4158166d81664dd36d12396a3
erik <erik.cr42@gmail.com>
2024-10-23 15:58:26 -0300

    Creates App

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { robots } from './robots';
-import CardList from './CardList';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
     <div>
-      <CardList robots={robots}/>
+      <App robots={robots}/>
     </div>
     </React.StrictMode>
 );
